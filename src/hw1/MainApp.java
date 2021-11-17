068ff63d6c43d5a044fe64b15aa0d81849aa3642
Ellias1991 <ilya.tumanow2015@yandex.ru>
2021-11-18 01:22:39 +0300

    hw1

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -2,8 +2,25 @@ package hw1;
 
 public class MainApp {
     public static void main(String[] args) {
-        // write your code here
+    printThreeWords();
+    checkSumSign();
     }
-}
+    public static void printThreeWords  (){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a=40;
+        int b=60;
+        int c = a + b;
+        if(c >=0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+
+        }
+    }
 
 }
