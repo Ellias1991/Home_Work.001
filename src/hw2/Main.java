9dce2480913351fa6d77bfb29f1f3e6f78155a41
Ellias1991 <ilya.tumanow2015@yandex.ru>
2021-11-21 19:26:03 +0300

    первое задание к уроку 2 готово

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -2,8 +2,15 @@ package hw2;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
-    }
+        checkingSum(5,6);
+            }
+            public static boolean checkingSum(int a, int b){
+        int sum = a+b;
+        if (sum>=10 && sum<=20) {
+            return true;
+        }else{
+            return false;
+        }
 }
 
 }
