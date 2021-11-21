3516794b5ed54c0eec68e4873260f25e994bf4ae
Ellias1991 <ilya.tumanow2015@yandex.ru>
2021-11-21 22:58:44 +0300

    домашняя работа к уроку 2 выполена полностью.

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -2,17 +2,61 @@ package hw2;
 
 public class Main {
     public static void main(String[] args) {
-        checkingSum(5,6);
-            }
-            public static boolean checkingSum(int a, int b){
-        int sum = a+b;
-        if (sum>=10 && sum<=20) {
+        isCheckingSum(5, 6);
+        //System.out.println((isCheckingSum(5, 6)));
+        plusOrMinus(100);
+        isPlusOrMinus(-9);
+        //System.out.println((isPlusOrMinus(-9)));
+        int a = 10;
+        printPhraseXTimes("Я люблю кодить на Java!", a);
+        int x=2024;
+        isLeapYear(x);
+
+    }
+
+    public static boolean isCheckingSum(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
             return true;
-        }else{
+        } else {
             return false;
         }
+    }
+
+    public static void plusOrMinus(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean isPlusOrMinus(int a) {
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void printPhraseXTimes(String Phrase, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(Phrase);
+        }
+
+    }
+
+    public static boolean isLeapYear(int x) {
+
+
+        if (x %400 == 0 && x%4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
 
-}
 
 
