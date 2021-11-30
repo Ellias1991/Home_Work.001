04ece521f1c25e0fe14aafbf9ec4524fea924843
Ellias1991 <ilya.tumanow2015@yandex.ru>
2021-12-01 01:44:33 +0300

    Задание выполнено полностью.

--- src/hw5/Employee.java
+++ src/hw5/Employee.java
@@ -9,22 +9,50 @@ public class Employee {
     private int salary;
     private int age;
 
-    public Employee(){
+    public Employee(Employee employee1) {
 
     }
-    public Employee(String name,String surname,String position,String email,String phone_number,int salary,int age){
-        this.name=name;
-        this.surname=surname;
-        this.position=position;
-        this.email=email;
-        this.phone_number=phone_number;
-        this.salary=salary;
-        this.age=age;
+
+    public Employee(String name, String surname, String position, String email, String phone_number, int salary, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.salary = salary;
+        this.age = age;
     }
 
 
-    public void info(){
-        System.out.printf("Employee information: %s %s\n%s\n%s\n%s\n%d\n%d\n",name,surname,position,email,phone_number,salary,age);
+    public void info() {
+        System.out.printf("Employee information: %s %s\n%s\n%s\n%s\n%d\n%d\n", name, surname, position, email, phone_number, salary, age);
     }
 
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
--- src/hw5/Main.java
+++ src/hw5/Main.java
@@ -2,22 +2,53 @@ package hw5;
 
 public class Main {
     public static void main(String[] args) {
-//        Employee employee1 = new Employee();
- //       employee1.name = "Ilya";
-  //      employee1.surname = "Tumanov";
- //       employee1.position = "Java developer";
- //       employee1.email = "ilya.tumanow2015@yandex.ru";
- //       employee1.phone_number = "89035973167";
- //       employee1.salary = 90000;
- //       employee1.age = 30;
 
-        Employee employee1 = new Employee("Ilya","Tumanov","Java developer",
-                "ilya.tumanow2015@yandex.ru","89035973167",
-                90000,30 );
+        Employee employee1 = new Employee("Ilya", "Tumanov", "Java developer",
+                "ilya.tumanow2015@yandex.ru", "89035973167",
+                120000, 30);
+        Employee employee2 = new Employee("Petr", "Petrov", "Python developer",
+                "petr.petrov@yandex.ru", "89999435677",
+                110000, 35);
+        Employee employee3 = new Employee("Ivan", "Ivanov", "Kotlin developer",
+                "ivan.ivanov@yandex.ru", "89999455678",
+                130000, 40);
+        Employee employee4 = new Employee("Denis", "Denisov", "Web designer",
+                "denis.denisov@yandex.ru", "89939455578",
+                140000, 45);
+        Employee employee5 = new Employee("Andrey", "Andreev", "PHP developer",
+                "andrey.andreev@yandex.ru", "89989455588",
+                150000, 50);
 
+        Employee[] employees = new Employee[]{
+                new Employee("Ilya", "Tumanov", "Java developer",
+                        "ilya.tumanow2015@yandex.ru", "89035973167",
+                        120000, 30),
+                new Employee("Petr", "Petrov", "Python developer",
+                        "petr.petrov@yandex.ru", "89999435677",
+                        110000, 35),
+                new Employee("Ivan", "Ivanov", "Kotlin developer",
+                        "ivan.ivanov@yandex.ru", "89999455678",
+                        130000, 40),
+                new Employee("Denis", "Denisov", "Web designer",
+                        "denis.denisov@yandex.ru", "89939455578",
+                        140000, 45),
+                new Employee("Andrey", "Andreev", "PHP developer",
+                        "andrey.andreev@yandex.ru", "89989455588",
+                        150000, 50),
+        };
+        //   employees[0].info();
+        //  employees[1].info();
+        //  employees[2].info();
+        //  employees[3].info();
+        //   employees[4].info();
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getAge() > 40) {
+                employees[i].info();
 
-
-        employee1.info();
+            }
+        }
     }
 
 }
+
+
